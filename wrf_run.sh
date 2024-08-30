8453fcec673049902e4cd415cac8f56c6c4c4c93
ehxa <2042918@student.uma.pt>
2024-08-30 15:15:58 +0100

    Update wrf_run.sh
    syntax error fixed

--- wrf_run.sh
+++ wrf_run.sh
@@ -6,21 +6,21 @@ host_cpu=$(nproc)
 
 read -p "How many CPUs? (Max: $host_cpu): " cpu
 
-while ! [[ $cpu =~ $re ] || [[ $cpu -gt host_cpu]]]; do
+while ! [[$cpu =~ $re]] || [[$cpu -gt host_cpu]]; do
     echo "Invalid option, try again."
     read -p "How many CPUs? (Max: $host_cpu): " cpu
 done
 
 read -p "How do you want to run it? Single-run (s) or Incrementally (i): " choice
 
-while [[ $choice != "i" && $choice != "s" ]]; do
+while [[$choice != "i" && $choice != "s"]]; do
     echo "Invalid option, try again."
     read -p "How do you want to run it? Single-run (s) or Incrementally (i): " choice
 done
 
 [[ $choice == "i" ]] && j=1 || j=$cpu
 
-while [[ $j -le $cpu ]]; do
+while [[$j -le $cpu]]; do
     echo "WRF with $j CPU(s) started"
     date
     mpirun -np $j ./wrf.exe
