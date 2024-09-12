1f6730d52378473d1a9c3275dfd297438fa82873
ehxa <2042918@student.uma.pt>
2024-09-12 14:48:03 +0100

    Added Docker support (in progress)

--- scripts/wrf_run.sh
+++ scripts/wrf_run.sh
@@ -14,24 +14,46 @@ while ! [[ $cpu =~ $re ]] || [[ $cpu -gt $host_cpu ]]; do
     read -p "How many CPUs? (Max: $host_cpu): " cpu
 done
 
-read -p "How do you want to run it? Single-run (s) or Incrementally (i): " choice
+read -p "How do you want to run it? Single-run (s) or Incrementally (i): " how
 
-while [[ $choice != "i" && $choice != "s" ]]; do
+while [[ $how != "i" && $how != "s" ]]; do
     echo "Invalid option, try again."
     echo ""
-    read -p "How do you want to run it? Single-run (s) or Incrementally (i): " choice
+    read -p "How do you want to run it? Single-run (s) or Incrementally (i): " how
+done
+
+read -p "Where do you want to run it? Native (n), Docker (d) or Mixed (m): " where
+
+while [[ $where != "n" && $where != "d" && $where != "m"  ]]; do
+    echo "Invalid option, try again."
+    echo ""
+    read -p "Where do you want to run it? Native (n), Docker (d) or Mixed (m): " where
 done
 
 echo ""
 
 {
-[[ $choice == "i" ]] && { j=1; echo "Beginning WRF with $cpu cycles in Incrementally mode"; } || { j=$cpu; echo "Beginning WRF with $cpu CPU(s) in Single-run mode"; }
+
+[[ $how == "i" ]] && { j=1; echo "Beginning WRF with $cpu cycles in Incrementally mode"; } || { j=$cpu; echo "Beginning WRF with $cpu CPU(s) in Single-run mode"; }
 
 while [[ $j -le $cpu ]]; do
     echo ""
+    . $HOME/wrf/gccvars.sh && cd $HOME/wrf/WRF/WRF/test/em_real
     echo "WRF with $j CPU(s) started"
     echo "Start: $(date)"
-    mpirun -np $j ./wrf.exe
+    [[ $where == "d" ]] && { 
+        systemctl --user start docker-desktop; 
+        sleep 20;
+        docker start ubuntu24.04-wrf-gcc;
+        #docker exec -it ubuntu24.04-wrf-gcc mpirun -np $j ./wrf.exe;
+        docker stop ubuntu24.04-wrf-gcc;
+        systemctl --user stop docker-desktop;
+    } || [[ $where == "n" ]] && { 
+        #mpirun -np $j ./wrf.exe;
+    }
+    || [[ $where == "m" ]] && { 
+        #mpirun -np $j ./wrf.exe;
+    }
     echo "Finish: $(date)"
     n=0 #count variable to print all rsl.out files
     while [[ $n -lt $j ]]; do
