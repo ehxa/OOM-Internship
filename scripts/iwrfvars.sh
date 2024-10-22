d8f95cbde7ead01493fcc5f59a121f771a2d9c66
ehxa <2042918@student.uma.pt>
2024-10-22 09:36:29 +0100

    Update iwrfvars.sh

--- scripts/iwrfvars.sh
+++ scripts/iwrfvars.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-export LD_LIBRARY_PATH=/opt/netcdf/lib:/opt/hdf5/lib:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/opt/netcdf/lib:/opt/libpng/lib:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/opt/netcdf/lib:/opt/netcdf/lib:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/opt/netcdf/lib:/opt/szip/lib:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/opt/netcdf/lib:/opt/udunitslib/lib:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/opt/netcdf/lib:/opt/zlib/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/opt/hdf5/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/opt/libpng/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/opt/netcdf/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/opt/szip/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/opt/udunitslib/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/opt/zlib/lib:$LD_LIBRARY_PATH
 . /opt/intel/oneapi/setvars.sh
