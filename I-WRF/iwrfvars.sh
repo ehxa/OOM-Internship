968a611e64a1da2a6a7a42f8e8747f13972e2374
ehxa <2042918@student.uma.pt>
2024-10-22 11:31:43 +0100

    Update iwrfvars.sh

--- I-WRF/iwrfvars.sh
+++ I-WRF/iwrfvars.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 . /opt/intel/oneapi/setvars.sh
 export LD_LIBRARY_PATH=/opt/hdf5/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/opt/jasper/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/opt/libpng/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/opt/netcdf/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/opt/szip/lib:$LD_LIBRARY_PATH
