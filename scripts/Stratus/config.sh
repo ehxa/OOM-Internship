ebc7d6d6709fe513071efc19e1a1e05f4bf14780
ehxa <2042918@student.uma.pt>
2025-01-29 09:55:43 +0000

    Update config.sh

--- scripts/Stratus/config.sh
+++ scripts/Stratus/config.sh
@@ -4,29 +4,30 @@
 #Version: 20250129 (WIP)
 #For the most recent updates check the repository: https://github.com/ehxa/OOM-Internship
 
-sudo mkdir /GEOG
-sudo mount /dev/sdc1 /GEOG
-sudo mkdir /WRF
-sudo mount /dev/sdb1 /WRF
-sudo mkdir /LOGS
-sudo mount /dev/sdd /LOGS
-sudo tar -xvf wrfcompiled.tar -C /home/ubuntu/
-sudo tar -xvzf wrf_input.tar.gz -C /home/ubuntu/wrf/ARM/
-sudo apt update
-sudo apt install -y make libxml2-dev m4 libcurl4-openssl-dev libtool csh
+sudo mkdir /GEOG && \
+sudo mount /dev/sdc1 /GEOG && \
+sudo mkdir /WRF && \
+sudo mount /dev/sdb1 /WRF && \
+sudo mkdir /LOGS && \
+sudo mount /dev/sdd /LOGS && \
+sudo tar -xvf wrfcompiled.tar -C /home/ubuntu/ && \
+sudo tar -xvzf wrf_input.tar.gz -C /home/ubuntu/wrf/ARM/ && \
+sudo apt update && \
+sudo apt install -y make libxml2-dev m4 libcurl4-openssl-dev libtool csh && \
 sudo apt-get -y install gcc-9 g++-9 gfortran-9 && \
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 9 && \
 sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-9 9 && \
 sudo update-alternatives --set gcc /usr/bin/gcc-9 && \
 sudo update-alternatives --set g++ /usr/bin/g++-9 && \
-sudo ln -s /usr/bin/gfortran-9 /usr/bin/gfortran
-gcc --version
-g++ --version
-gfortran --version
-cd $HOME/wrf/ 
-wget https://raw.githubusercontent.com/ehxa/OOM-Internship/refs/heads/main/scripts/General/wrf_run.sh
-chmod u+x wrf_run.sh
-for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove -y $pkg; done && sudo apt-get update -y && sudo apt-get install -y ca-certificates curl && sudo install -m 0755 -d /etc/apt/keyrings && sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc && sudo chmod a+r /etc/apt/keyrings/docker.asc && echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null && sudo apt-get update -y && sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin && sudo groupadd docker && sudo usermod -aG docker $USER && newgrp docker
-cd $HOME/wrf/WRF/WRF/run 
-mv namalist.input namelist.input.backup
-ln -s /home/ubuntu/wrf/ARM/wrf_tmp/* .
+sudo ln -s /usr/bin/gfortran-9 /usr/bin/gfortran && \
+gcc --version && \
+g++ --version && \
+gfortran --version && \
+cd $HOME/wrf/ && \
+wget https://raw.githubusercontent.com/ehxa/OOM-Internship/refs/heads/main/scripts/General/wrf_run.sh && \
+chmod u+x wrf_run.sh && \
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove -y $pkg; done && sudo apt-get update -y && sudo apt-get install -y ca-certificates curl && sudo install -m 0755 -d /etc/apt/keyrings && sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc && sudo chmod a+r /etc/apt/keyrings/docker.asc && echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null && sudo apt-get update -y && sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin && sudo groupadd docker && sudo usermod -aG docker $USER && newgrp docker && \
+cd $HOME/wrf/WRF/WRF/run && \
+mv namalist.input namelist.input.backup && \
+ln -s /home/ubuntu/wrf/ARM/wrf_tmp/* . && \
+cd $HOME/wrf
