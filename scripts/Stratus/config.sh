5f04e43c51e6c8b2f1e98ada688d11c91b7d7abd
ehxa <2042918@student.uma.pt>
2025-01-31 10:27:32 +0000

    Configs update

--- scripts/Stratus/config.sh
+++ scripts/Stratus/config.sh
@@ -28,6 +28,8 @@ cd $HOME/wrf/ && \
 wget https://raw.githubusercontent.com/ehxa/OOM-Internship/refs/heads/main/scripts/Stratus/wrf_run.sh && \
 chmod u+x wrf_run.sh && \
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove -y $pkg; done && sudo apt-get update -y && sudo apt-get install -y ca-certificates curl && sudo install -m 0755 -d /etc/apt/keyrings && sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc && sudo chmod a+r /etc/apt/keyrings/docker.asc && echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null && sudo apt-get update -y && sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin && sudo groupadd docker && sudo usermod -aG docker ubuntu && newgrp docker && \
+sudo systemctl stop docker && \
+sudo systemctl stop docker.socket && \
 cd $HOME/wrf/WRF/WRF/run && \
 mv namelist.input namelist.input.backup && \
 sudo ln -s /home/ubuntu/wrf/ARM/wrf_tmp/* .
