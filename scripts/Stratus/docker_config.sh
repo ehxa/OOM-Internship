5560cd360b9efe5dc8f656a74644ff9d05e2b228
ehxa <2042918@student.uma.pt>
2025-02-03 12:07:25 +0000

    Update docker_config.sh

--- scripts/Stratus/docker_config.sh
+++ scripts/Stratus/docker_config.sh
@@ -5,8 +5,7 @@
 #For the most recent updates check the repository: https://github.com/ehxa/OOM-Internship
 
 docker pull ehxa/oom:ubuntu24.04-gcc-20250130-1250-configured
-docker run -d --name ubuntu24.04-wrf-gcc ehxa/oom:ubuntu24.04-gcc-20250130-1250-configured
+docker run -d --name ubuntu24.04-wrf-gcc ehxa/oom:ubuntu24.04-gcc-20250130-1250-configured tail -f /dev/null
 docker cp /WRF/wrf_input.tar.gz ubuntu24.04-wrf-gcc:/WRF/
-docker start ubuntu24.04-wrf-gcc
 docker exec ubuntu24.04-wrf-gcc bash -c "sudo tar -xvzf /WRF/wrf_input.tar.gz -C /home/swe/wrf/ARM/ && sudo ln -s /home/swe/wrf/ARM/wrf_tmp/* ."
 docker stop ubuntu24.04-wrf-gcc
