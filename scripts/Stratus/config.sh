f553f19a369843258e02876084f4daa7207e1bd6
ehxa <2042918@student.uma.pt>
2025-02-03 14:20:27 +0000

    Update config.sh

--- scripts/Stratus/config.sh
+++ scripts/Stratus/config.sh
@@ -24,9 +24,7 @@ sudo ln -s /usr/bin/gfortran-9 /usr/bin/gfortran && \
 gcc --version && \
 g++ --version && \
 gfortran --version && \
-cd $HOME && \
-wget https://raw.githubusercontent.com/ehxa/OOM-Internship/refs/heads/main/scripts/Stratus/wrf_run.sh && \
-chmod u+x wrf_run.sh && \
+cd $HOME && wget https://raw.githubusercontent.com/ehxa/OOM-Internship/refs/heads/main/scripts/Stratus/wrf_run.sh && chmod u+x wrf_run.sh && \
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove -y $pkg; done && sudo apt-get update -y && sudo apt-get install -y ca-certificates curl && sudo install -m 0755 -d /etc/apt/keyrings && sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc && sudo chmod a+r /etc/apt/keyrings/docker.asc && echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null && sudo apt-get update -y && sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin && sudo groupadd docker && sudo usermod -aG docker ubuntu && newgrp docker && \
 sudo docker pull ehxa/oom:ubuntu24.04-gcc-20250130-1250-configured && \
 sudo docker run -d --name ubuntu24.04-wrf-gcc ehxa/oom:ubuntu24.04-gcc-20250130-1250-configured tail -f /dev/null && \
