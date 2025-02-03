61df4fb721e4b5597528565332a9dab52bd83a30
ehxa <2042918@student.uma.pt>
2025-02-03 14:29:39 +0000

    Update config.sh

--- scripts/Stratus/config.sh
+++ scripts/Stratus/config.sh
@@ -23,8 +23,8 @@ sudo update-alternatives --set g++ /usr/bin/g++-9 && \
 sudo ln -s /usr/bin/gfortran-9 /usr/bin/gfortran && \
 gcc --version && \
 g++ --version && \
-gfortran --version && \
-cd $HOME && wget https://raw.githubusercontent.com/ehxa/OOM-Internship/refs/heads/main/scripts/Stratus/wrf_run.sh && chmod u+x wrf_run.sh && \
+gfortran --version
+cd $HOME && wget https://raw.githubusercontent.com/ehxa/OOM-Internship/refs/heads/main/scripts/Stratus/wrf_run.sh && chmod u+x wrf_run.sh
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove -y $pkg; done && sudo apt-get update -y && sudo apt-get install -y ca-certificates curl && sudo install -m 0755 -d /etc/apt/keyrings && sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc && sudo chmod a+r /etc/apt/keyrings/docker.asc && echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null && sudo apt-get update -y && sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin && sudo groupadd docker && sudo usermod -aG docker ubuntu && newgrp docker && \
 sudo docker pull ehxa/oom:ubuntu24.04-gcc-20250130-1250-configured && \
 sudo docker run -d --name ubuntu24.04-wrf-gcc ehxa/oom:ubuntu24.04-gcc-20250130-1250-configured tail -f /dev/null && \
@@ -34,6 +34,6 @@ sudo docker stop ubuntu24.04-wrf-gcc && \
 sudo systemctl stop docker && \
 sudo systemctl stop docker.socket && \
 cd $HOME/wrf/WRF/WRF/run && \
-mv namelist.input namelist.input.backup && \
-sudo ln -s /home/ubuntu/wrf/ARM/wrf_tmp/* /home/ubuntu/wrf/WRF/WRF/run
+mv namelist.input namelist.input.backup
+ln -s /home/ubuntu/wrf/ARM/wrf_tmp/* /home/ubuntu/wrf/WRF/WRF/run
 cd $HOME
