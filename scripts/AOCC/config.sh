293a3716c5990c8b6da43a40db0bd9cc41b77ba6
ehxa <2042918@student.uma.pt>
2025-02-14 11:43:03 +0000

    Update config.sh

--- scripts/AOCC/config.sh
+++ scripts/AOCC/config.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #AOCC Installation Guide (amd64)
 
+#docker run -d --name ubuntu24.04-wrf-aocc ehxa/oom:ubuntu24.04-aocc-v0.1 tail -f /dev/null
 export SPACK_ROOT=$HOME/spack
 git clone -c feature.manyFiles=true https://github.com/spack/spack.git ${SPACK_ROOT}
 . ${SPACK_ROOT}/share/spack/setup-env.sh
@@ -8,4 +9,4 @@ spack install aocc +license-agreed
 spack cd -i aocc
 spack compiler add $PWD
 spack load aocc
-spack install wrf %aocc@4.4.2 build_type=dmpar ^openmpi fabrics=cma,ucx
+spack install wrf %aocc@4.4.2 build_type=dmpar #^openmpi fabrics=cma,ucx
