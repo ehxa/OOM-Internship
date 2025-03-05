c21e2eb5ac949bd8e5c0c6aeb0c398ca7cf4a135
ehxa <2042918@student.uma.pt>
2025-03-05 08:08:05 +0000

    mount disks correction

--- scripts/Stratus/mount_disks.sh
+++ scripts/Stratus/mount_disks.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+sudo mkdir -p /WRF
+sudo mkdir -p /GEOG
+sudo mkdir -p /LOGS
 sudo mount /dev/sdb1 /WRF || true
 sudo mount /dev/sdc1 /GEOG || true
 sudo mount /dev/sdd1 /LOGS || true
--- scripts/Stratus/mount_disks_worker.sh
+++ scripts/Stratus/mount_disks_worker.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 read -p "What is the IP address of the controller node? " controller
+sudo mkdir -p /WRF
+sudo mkdir -p /GEOG
+sudo mkdir -p /LOGS
 sudo mount $controller:/WRF /WRF || true
 sudo mount $controller:/GEOG /GEOG || true
 sudo mount $controller:/LOGS /LOGS || true
-lsblk
 echo "Disks mounted."
