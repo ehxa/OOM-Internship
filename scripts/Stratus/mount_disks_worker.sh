290c883d4aa03b4a3aac6068e843853c159ad201
ehxa <2042918@student.uma.pt>
2025-03-14 15:00:33 +0000

    Update mount_disks_worker.sh

--- scripts/Stratus/mount_disks_worker.sh
+++ scripts/Stratus/mount_disks_worker.sh
@@ -2,10 +2,14 @@
 read -p "What is the IP address of the controller node? " controller
 sudo mkdir -p /WRF /GEOG /LOGS
 sudo mount $controller:/WRF /WRF || echo "Failed to mount /WRF"
+echo "WRF:"
 ls /WRF
+echo ""
 sudo mount $controller:/GEOG /GEOG || echo "Failed to mount /GEOG"
+echo "GEOG:"
 ls /GEOG
 sudo mount $controller:/LOGS /LOGS || echo "Failed to mount /GEOG"
+echo "LOGS:"
 ls /LOGS
 lsblk
 echo "Disks mounted."
