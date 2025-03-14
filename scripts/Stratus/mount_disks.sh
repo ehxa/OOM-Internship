74997180f4b66efb71a53b9bb344d9ca8d208a9f
ehxa <2042918@student.uma.pt>
2025-03-14 09:58:19 +0000

    Shows mount output

--- scripts/Stratus/mount_disks.sh
+++ scripts/Stratus/mount_disks.sh
@@ -5,6 +5,9 @@ read -p "Select GEOG device letter: " geog
 read -p "Select LOGS device letter: " logs
 sudo mkdir -p /WRF /GEOG /LOGS
 sudo mount /dev/sd${wrf}1 /WRF || echo "Failed to mount /dev/sd${wrf}1"
+ls /WRF
 sudo mount /dev/sd${geog}1 /GEOG || echo "Failed to mount /dev/sd${geog}1"
+ls /GEOG
 sudo mount /dev/sd${logs}1 /LOGS || echo "Failed to mount /dev/sd${logs}1"
+ls /LOGS
 lsblk
--- scripts/Stratus/mount_disks_worker.sh
+++ scripts/Stratus/mount_disks_worker.sh
@@ -2,6 +2,10 @@
 read -p "What is the IP address of the controller node? " controller
 sudo mkdir -p /WRF /GEOG /LOGS
 sudo mount $controller:/WRF /WRF || echo "Failed to mount /WRF"
+ls /WRF
 sudo mount $controller:/GEOG /GEOG || echo "Failed to mount /GEOG"
+ls /GEOG
 sudo mount $controller:/LOGS /LOGS || echo "Failed to mount /GEOG"
+ls /LOGS
+lsblk
 echo "Disks mounted."
