eb4dc2b988dc7f07ba1d364181be0b47cf4abad5
ehxa <2042918@student.uma.pt>
2025-05-06 21:53:37 +0100

    WIP testing multi-worker config on k8s

--- Kubernetes/wrf_run_host.sh
+++ Kubernetes/wrf_run_host.sh
@@ -3,7 +3,7 @@ kubectl exec wrf-arm-master -- bash -c "
 . /home/swe/wrf/gccvars.sh &&
 ln -s /home/swe/wrf/WRF/WRF/run/* /mnt/data || true &&
 cd /mnt/data &&
-timeout 60s mpirun -np 2 --hostfile hosts.txt ./wrf.exe || true &&
+timeout 60s mpirun -np 4 --hostfile hosts.txt ./wrf.exe || true &&
 echo 'WRF job completed' &&
 echo '' &&
 echo 'rsl.error.0000:' &&
